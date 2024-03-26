e6838082868f58691c094b20f554c4cc3d2d70e3
The Fro Code <cmukiri808@gmail.com>
2024-03-26 09:00:37 +0300

    UI: Navbar

---
+++ src/app/(root)/components/header.tsx
@@ -0,0 +1,32 @@
+import { Montserrat, Delius_Unicase } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
+
+const deliusUnicase = Delius_Unicase({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export default function Header() {
+  return (
+    <header className="w-full py-5">
+      <nav className="flex justify-between px-32 items-center">
+        <span
+          className={`${deliusUnicase.className} font-bold text-2xl underlined`}
+        >
+          The Fro Code
+        </span>
+
+        <ul
+          className={`flex justify-center gap-12 ${montserrat.className} font-medium text-lg`}
+        >
+          <li>HOME</li>
+          <li>STACK</li>
+          <li>EXPERIENCE</li>
+          <li>PROJECTS</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-//import { Inter } from "next/font/google";
 import "../../app/globals.css";
-
-//const inter = Inter({ subsets: ["latin"] });
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <div>{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
