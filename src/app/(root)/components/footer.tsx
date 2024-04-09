938fd5490d1f6910eb16c500531d5bcefc072a6a
The Fro Code <cmukiri808@gmail.com>
2024-04-09 16:29:20 +0300

    Fix footer

--- src/app/(root)/components/footer.tsx
+++ src/app/(root)/components/footer.tsx
@@ -7,9 +7,9 @@ export default function Footer() {
     }
   };
   return (
-    <footer className="flex px-16 py-8 bg-black text-white gap-16">
-      <div className="flex flex-col gap-4 w-1/2">
-        <p className="mr-10">
+    <footer className="flex px-4 md:px-16 py-8 bg-black text-white gap-6 md:gap-16 flex-wrap md:flex-nowrap">
+      <div className="flex flex-col gap-4 w-full md:w-1/2">
+        <p className="md:mr-10 text-sm md:text-base">
           Lörem ipsum od ohet dilogi. Bell trabel, samuligt, ohöbel utom diska.
           Jinesade bel när feras redorade i belogi. FAR paratyp i muvåning, och
           pesask vyfisat. Viktiga poddradio har un mad och inde.
@@ -31,47 +31,53 @@ export default function Footer() {
           </a>
         </div>
       </div>
-      <div className="flex flex-col gap-2">
-        <p className="font-bold">Pages</p>
-        <button
-          onClick={() => scrollToSection("home")}
-          className="text-xs text-left"
-        >
-          Home
-        </button>
-        <button
-          onClick={() => scrollToSection("stack")}
-          className="text-xs text-left"
-        >
-          Tech Stack
-        </button>
-        <button
-          onClick={() => scrollToSection("experience")}
-          className="text-xs text-left"
-        >
-          Experience
-        </button>
-        <button
-          onClick={() => scrollToSection("projects")}
-          className="text-xs text-left"
-        >
-          Projects
-        </button>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p className="font-bold">Contact</p>
-        <p className="text-xs">
-          <i className="bi bi-telephone-fill text-white mr-2" />
-          (+254) 700 000808
-        </p>
-        <p className="text-xs">
-          <i className="bi bi-envelope text-white mr-2" />
-          cmukiri808@gmail.com
-        </p>
-        <p className="text-xs">
-          <i className="bi bi-globe text-white mr-2" />
-          thefrocode.github.io
-        </p>
+      <div className="flex gap-6 md:gap-16 w-full md:w-1/2">
+        <div className="flex flex-col gap-2 w-1/3">
+          <p className="text-sm font-semibold md:text-base md:font-bold">
+            Pages
+          </p>
+          <button
+            onClick={() => scrollToSection("home")}
+            className="text-xs text-left"
+          >
+            Home
+          </button>
+          <button
+            onClick={() => scrollToSection("stack")}
+            className="text-xs text-left"
+          >
+            Tech Stack
+          </button>
+          <button
+            onClick={() => scrollToSection("experience")}
+            className="text-xs text-left"
+          >
+            Experience
+          </button>
+          <button
+            onClick={() => scrollToSection("projects")}
+            className="text-xs text-left"
+          >
+            Projects
+          </button>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-sm font-semibold md:text-base md:font-bold">
+            Contact
+          </p>
+          <p className="text-xs">
+            <i className="bi bi-telephone-fill text-white mr-2" />
+            (+254) 700 000808
+          </p>
+          <p className="text-xs">
+            <i className="bi bi-envelope text-white mr-2" />
+            cmukiri808@gmail.com
+          </p>
+          <p className="text-xs">
+            <i className="bi bi-globe text-white mr-2" />
+            thefrocode.github.io
+          </p>
+        </div>
       </div>
     </footer>
   );
